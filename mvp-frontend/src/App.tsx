a2fba0323a1087f83f04b154e7297e22d3a128ab
Cjache Kang <cjache3000@hotmail.com>
2024-11-08 16:06:13 -0800

    initial mvp code

--- mvp-frontend/src/App.tsx
+++ mvp-frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '@/pages/Home';
+import About from '@/pages/About';
 
 const App: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Hello, Vite + React + TypeScript + Tailwind!</h1>
-    </div>
+    <Router>
+      <div className="flex h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/contact" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
